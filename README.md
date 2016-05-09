ac362b00bac1c1bd0ce980e45ff455ac84c1e9e2
Jorge Munoz <elkidster@gmail.com>
2016-05-09 18:22:51 +0000

    Create test data on_start

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# locust
+Keystone performance test for revocation events
+===============================================
+
 Performance test script for Keystone using locust to test revocation events
 
+The KeystoneTaskSet include two different tasks.
+    Task 'start': Runs twices as much as Task 'increase_revocation_records'.
+        Authenticates -> Validates(x10) -> revoke token
+    Task 'increase_revocation_records':
+        Gradually increate the number of revocation events in Keystone by authenticating the 'test_user' and revoking its token. (x100)
+
 Setup:
+~~~~~~
+
 pip install locustio
 
-Run:
+The environment variables ADMIN_USER, ADMIN_PASSWORD, ADMIN_DOMAIN_NAME, ADMIN_DOMAIN_ID will need to be set in order to create the test data in Keystone.
+
+The following example will generate 1000+ revocation events.
+Run example:
+export ADMIN_USER=admin
+export ADMIN_PASSWORD=admin
+export ADMIN_DOMAIN_NAME=Default
+export ADMIN_PROJECT_ID=a9538f8a717f4215a90c9f41220499cb
+
 locust -f locustfile.py --host=http://127.0.0.1 --num-request=2500
+
--- locustfile.py
+++ locustfile.py
@@ -1,21 +1,47 @@
 import copy
 import json
+import os
 import uuid
 
 from locust import HttpLocust, TaskSet, task
 
-TOKEN_PATH = '/v3/auth/tokens'
-DOMAIN_PATH = '/v3/domains'
 HEADERS = {'content-type': 'application/json'}
+PROJECT_PATH = '/v3/projects'
+TOKEN_PATH = '/v3/auth/tokens'
+USER_PATH = '/v3/users'
 
 class KeystoneTaskSet(TaskSet):
+    """ Performance Task
+
+    The KeystoneTaskSet include two different tasks.
+    Task 'start': Runs twices as much as Task 'increase_revocation_records'.
+        Authenticates -> Validates(x10) -> revoke token
+    Task 'increase_revocation_records':
+        Gradually increate the number of revocation events in Keystone by
+        authenticating the 'test_user' and revoking the token. (x100)
+    """
 
     def on_start(self):
-        HEADERS['X-Auth-Token'] = self._get_token(
-            'admin', 'admin', 'Default',
-            'e07767c6cb844c939b78add51c76f9b5')
+        """ Setup admin and test user. """
+        admin_user = os.environ['ADMIN_USER']
+        admin_password = os.environ['ADMIN_PASSWORD']
+        admin_domain_name = os.environ['ADMIN_DOMAIN_NAME']
+        admin_project_id = os.environ['ADMIN_PROJECT_ID']
+        HEADERS['X-Auth-Token'] = self._get_token(admin_user,
+                                                  admin_password,
+                                                  admin_domain_name,
+                                                  project_id=admin_project_id)
+        # Create test user
+        self.username = 'test_user'
+        self.password = 'Password1'
+        self.user_domain_id = 'default'
+        self.user_domain_name = 'Default'
+        self.project_id = self._create_project()['project']['id']
+        self._create_user(self.username, self.password, self.user_domain_id,
+                          self.project_id)
 
     def _get_token(self, username, password, user_domain, project_id=None):
+        """ Authenticate user and return token. """
         request = {
             "auth": {
                 "identity": {
@@ -46,25 +72,56 @@ class KeystoneTaskSet(TaskSet):
         if response.status_code == 201:
             return response.headers['X-Subject-Token']
         else:
-            raise SystemExit(response.status_code)
+            msg = "Failed to authenticate %s user. Status %s" % (username,
+                response.status_code)
+            raise SystemExit(msg)
 
-    def _create_domain(self):
+    def _create_user(self, username, password, domain_id, project_id):
+        """ Create user request in Keystone. """
         request = {
-            "domain": {
+            "user": {
+                "name": username,
+                "password": password,
+                "domain_id": domain_id,
+                "default_project_id": project_id,
                 "description": "description",
-                "enabled": False,
-                "name":  uuid.uuid4().hex,
+                "email": "test@example.com",
+                "enabled": True,
             }
         }
-        response = self.client.post(DOMAIN_PATH, data=json.dumps(request),
+        response = self.client.post(USER_PATH, data=json.dumps(request),
                                     headers=HEADERS)
-        return response
+        if response.status_code == 409:
+            return
+        elif response.status_code == 201:
+            return response.json()
+        else:
+            raise SystemExit("Failed to create test user.")
+
+    def _create_project(self):
+        """ Create project in Keystone. """
+        request = {
+            "project": {
+                "description": "description",
+                "enabled": True,
+                "name":  uuid.uuid4().hex,
+                "domain_id": "default",
+            }
+        }
+        response = self.client.post(PROJECT_PATH, data=json.dumps(request),
+                                    headers=HEADERS)
+
+        if response.status_code == 201:
+            return response.json()
+        else:
+            raise SystemExit("Failed to create project.")
 
     @task(2)
     def start(self):
         headers = copy.deepcopy(HEADERS)
-        headers['X-Subject-Token'] = self._get_token('test_user', 'Password1',
-                                                     'test_domain')
+        headers['X-Subject-Token'] = self._get_token(self.username,
+                                                     self.password,
+                                                     self.user_domain_name)
         for i in range(10):
             self.client.get(TOKEN_PATH, headers=headers)
         self.client.delete(TOKEN_PATH, headers=headers)
@@ -74,9 +131,9 @@ class KeystoneTaskSet(TaskSet):
         headers = copy.deepcopy(HEADERS)
 
         for i in range(100):
-            headers['X-Subject-Token'] = self._get_token('test_user',
-                                                         'Password1',
-                                                         'test_domain')
+            headers['X-Subject-Token'] = self._get_token(self.username,
+                                                         self.password,
+                                                         self.user_domain_name)
             self.client.delete(TOKEN_PATH, headers=headers)
 
 
